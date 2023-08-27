528fa6939c589415b0b05337146c16d82274a341
ganesh nikhil <73976037+ganeshnikhil@users.noreply.github.com>
2023-08-27 13:44:36 +0530

    Delete c_print_shapes directory

--- c_print_shapes/add_int.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-   int a,b;
-   a=100;
-   b=200;
-   printf("%d + %d = %d\n",a,b,a+b);
-}
--- c_print_shapes/print_input.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-   int num;
-   printf("Enter the number:");
-   scanf("%d",&num);
-   printf("\nyou Entered:%d\n",num);
-}
